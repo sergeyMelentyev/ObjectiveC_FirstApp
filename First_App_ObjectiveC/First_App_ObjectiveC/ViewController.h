866b6d5dbdde45956a351159037aaba046022df4
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-01-21 10:57:42 +0300

    new version

---
+++ First_App_ObjectiveC/First_App_ObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  First_App_ObjectiveC
+//
+//  Created by Админ on 21.01.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ First_App_ObjectiveC/First_App_ObjectiveCTests/First_App_ObjectiveCTests.m
@@ -0,0 +1,39 @@
+//
+//  First_App_ObjectiveCTests.m
+//  First_App_ObjectiveCTests
+//
+//  Created by Админ on 21.01.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface First_App_ObjectiveCTests : XCTestCase
+
+@end
+
+@implementation First_App_ObjectiveCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
